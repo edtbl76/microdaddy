939cf463f6d1518c93630a43b7d4395961a975db
Ed and Gravy <me@emangini.com>
2023-06-11 01:04:38 -0400

    Added recommendation-service and product-composite-service

---
+++ product-composite-service/src/test/java/org/tbl/microdaddy/composite/product/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package org.tbl.microdaddy.composite.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+    @Test
+    void contextLoads() { }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "microdaddy"
 include("product-service", "review-service", "recommendation-service", "product-composite-service")
+
