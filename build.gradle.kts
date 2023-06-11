5575852806afd893ac8af05140ee751d5a33c632
Ed and Gravy <me@emangini.com>
2023-06-10 23:46:42 -0400

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+subprojects {
+    group = "org.tbl"
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        testImplementation(platform("org.junit:junit-bom:5.9.1"))
+        testImplementation("org.junit.jupiter:junit-jupiter")
+    }
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
