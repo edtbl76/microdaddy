8237fddacfd67a811abefe0d031731ae0a144929
Ed and Gravy <me@emangini.com>
2023-06-11 00:26:33 -0400

    product-service and review-service

---
+++ product-service/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.0"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.register("prepareKotlinBuildScriptModel") {}
