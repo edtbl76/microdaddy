10733cd5fe5f9a53ce29193bdef11bbe3aa06589
Ed and Gravy <me@emangini.com>
2023-06-14 08:59:03 -0400

    Excluded DataSourceAutoConfiguration.class from @SpringBootApplication in ReviewServiceApplication in order to get past the "failed to configure a datasource 'url' attribute is not specified" errors. (This was causing tests to fail)

--- review-service/src/main/java/org/tbl/microdaddy/review/ReviewServiceApplication.java
+++ review-service/src/main/java/org/tbl/microdaddy/review/ReviewServiceApplication.java
@@ -2,9 +2,10 @@ package org.tbl.microdaddy.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan("org.tbl")
 public class ReviewServiceApplication {
 
--- review-service/src/test/java/org/tbl/microdaddy/review/ReviewServiceApplicationTests.java
+++ review-service/src/test/java/org/tbl/microdaddy/review/ReviewServiceApplicationTests.java
@@ -3,7 +3,6 @@ package org.tbl.microdaddy.review;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
