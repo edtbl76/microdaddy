fb274a5727b9f7a7e2defbb052b67b0d5ab14a85
Ed and Gravy <me@emangini.com>
2023-06-14 00:41:52 -0400

    CreatedRepositories: extended CrudRepository for Recommendation, Review extending PagingAndSortingRepository for Product.
    Completed Persistence layer.

---
+++ product-service/src/main/java/org/tbl/microdaddy/core/product/persistence/ProductRepository.java
@@ -0,0 +1,11 @@
+package org.tbl.microdaddy.core.product.persistence;
+
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository  extends PagingAndSortingRepository <ProductEntity, String> {
+
+    Optional<ProductEntity> findByProductId(int productId);
+}
---
+++ recommendation-service/src/main/java/org/tbl/microdaddy/core/recommendation/persistence/RecommendationRepository.java
@@ -0,0 +1,10 @@
+package org.tbl.microdaddy.core.recommendation.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+
+    List<RecommendationEntity> findByProductId(int productId);
+}
