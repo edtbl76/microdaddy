a021cc2313430f3c12aec50cf9d6bfbcd3fffa9c
Ed and Gravy <me@emangini.com>
2023-06-16 00:15:00 -0400

    Updated service driver coder and concrete implementation; added tests and logback.

--- recommendation-service/src/main/java/org/tbl/microdaddy/core/recommendation/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/org/tbl/microdaddy/core/recommendation/RecommendationServiceApplication.java
@@ -1,14 +1,48 @@
 package org.tbl.microdaddy.core.recommendation;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.IndexResolver;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
+import org.tbl.microdaddy.core.recommendation.persistence.RecommendationEntity;
 
+@Slf4j
 @SpringBootApplication
 @ComponentScan("org.tbl.microdaddy")
 public class RecommendationServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RecommendationServiceApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(RecommendationServiceApplication.class, args);
+
+        String mongoDbHost = context.getEnvironment().getProperty("spring.data.mongodb.host");
+        String mongoDbPort = context.getEnvironment().getProperty("spring.data.mongodb.port");
+        log.info("Connected to MongoDb: " + mongoDbPort + ":" + mongoDbHost);
     }
+
+    @Autowired
+    MongoOperations mongoTemplate;
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void initIndicesAfterStartup() {
+        MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext =
+                mongoTemplate.getConverter().getMappingContext();
+
+        IndexResolver resolver = new MongoPersistentEntityIndexResolver(mappingContext);
+
+        IndexOperations indexOperations = mongoTemplate.indexOps(RecommendationEntity.class);
+        resolver.resolveIndexFor(RecommendationEntity.class).forEach(indexOperations::ensureIndex);
+    }
+
+
 }
