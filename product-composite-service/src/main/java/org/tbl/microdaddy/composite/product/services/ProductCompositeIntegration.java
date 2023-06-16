3162c604545594f4ab6c29c74cd67c2037e33c62
Ed and Gravy <me@emangini.com>
2023-06-16 13:19:30 -0400

    updated product-composite-service tests are still failing w/ dataSource issues. these are likely review-service

--- product-composite-service/src/main/java/org/tbl/microdaddy/composite/product/services/ProductCompositeIntegration.java
+++ product-composite-service/src/main/java/org/tbl/microdaddy/composite/product/services/ProductCompositeIntegration.java
@@ -1,7 +1,5 @@
 package org.tbl.microdaddy.composite.product.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
