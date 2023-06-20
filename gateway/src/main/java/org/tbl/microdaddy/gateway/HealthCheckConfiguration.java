3bd56ccc374c7c2b2fa0a609df82466cfae07087
Ed and Gravy <me@emangini.com>
2023-06-20 00:15:38 -0400

    Added gateway module: - For Spring Cloud Gateway as an edge service - Updated master settings - created Kotlin DSL typed build.gradle. - Created Dockerfile (like microservices) - updated docker compose files w/ gateway and removed the externally facing ports for product-composite and eureka. - added configuration including all of the gateway routes (replacing the ports) - updated product-composite app.yml config to support the use of spring cloud gateway. - updated e2e test to support the new edge server location, as well as to test for the proper OpenAPI exposure.

---
+++ gateway/src/main/java/org/tbl/microdaddy/gateway/HealthCheckConfiguration.java
@@ -0,0 +1,54 @@
+package org.tbl.microdaddy.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.CompositeReactiveHealthContributor;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.ReactiveHealthContributor;
+import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static java.util.logging.Level.FINE;
+
+@Slf4j
+@Configuration
+public class HealthCheckConfiguration {
+
+    private WebClient webClient;
+
+    @Autowired
+    public HealthCheckConfiguration(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
+    @Bean
+    ReactiveHealthContributor healthcheckMicroservices() {
+        final Map<String, ReactiveHealthIndicator> healthCheckRegistry = new LinkedHashMap<>();
+
+        healthCheckRegistry.put("product", () -> getHealth("http://product"));
+        healthCheckRegistry.put("recommendation", () -> getHealth("http://recommendation"));
+        healthCheckRegistry.put("review", () -> getHealth("http://review"));
+        healthCheckRegistry.put("product-composite", () -> getHealth("http://product-composite"));
+
+        return CompositeReactiveHealthContributor.fromMap(healthCheckRegistry);
+    }
+
+    private Mono<Health> getHealth(String baseUrl) {
+        String url = baseUrl + "/actuator/health";
+        log.debug("Calling health API at URL: {}", url);
+
+        return webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(String.class)
+                .map(str -> new Health.Builder().up().build())
+                .onErrorResume(ex -> Mono.just(new Health.Builder().down(ex).build()))
+                .log(log.getName(), FINE);
+    }
+}
