769680657da8834c57077090fc649083ee7fa37f
Ed and Gravy <me@emangini.com>
2023-06-24 16:36:22 -0400

    - restricted access to discovery server, updates client configurations to allow registration

---
+++ discovery/src/main/java/org/tbl/microdaddy/discovery/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package org.tbl.microdaddy.discovery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    private final static String USER_ROLES = "USER";
+
+    private final String username;
+    private final String password;
+
+    @Autowired
+    public SecurityConfiguration(
+            @Value("${app.eureka-username}") String username,
+            @Value("${app.eureka-password}") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username(username)
+                .password(password)
+                .roles(USER_ROLES)
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+}
