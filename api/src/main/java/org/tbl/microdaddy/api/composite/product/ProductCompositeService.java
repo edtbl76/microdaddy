c5d929653c9c38ce30e115a669bffbf606702928
Ed and Gravy <me@emangini.com>
2023-07-03 19:07:27 -0400

    added support for optional parameters, delay and faultPercent ot product-composite and product apis.

--- api/src/main/java/org/tbl/microdaddy/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/org/tbl/microdaddy/api/composite/product/ProductCompositeService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import reactor.core.publisher.Mono;
 
@@ -58,7 +59,10 @@ public interface ProductCompositeService {
     @GetMapping(
             value = "/product-composite/{productId}",
             produces = "application/json")
-    Mono<ProductAggregate> getProduct(@PathVariable int productId);
+    Mono<ProductAggregate> getProduct(
+            @PathVariable int productId,
+            @RequestParam(value = "delay", required = false, defaultValue = "0") int delay,
+            @RequestParam(value = "faultPercent", required = false, defaultValue = "0") int faultPercent);
 
 
     /**
--- api/src/main/java/org/tbl/microdaddy/api/core/product/ProductService.java
+++ api/src/main/java/org/tbl/microdaddy/api/core/product/ProductService.java
@@ -2,6 +2,7 @@ package org.tbl.microdaddy.api.core.product;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 
 public interface ProductService {
@@ -16,7 +17,10 @@ public interface ProductService {
     @GetMapping(
             value = "/product/{productId}",
             produces = "application/json")
-    Mono<Product> getProduct(@PathVariable int productId);
+    Mono<Product> getProduct(
+            @PathVariable int productId,
+            @RequestParam(value = "delay", required = false, defaultValue = "0") int delay,
+            @RequestParam(value = "faultPercent", required = false, defaultValue = "0") int faultPercent);
 
     Mono<Void> deleteProduct(int productId);
 
