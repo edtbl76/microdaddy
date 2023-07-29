f386b7e273ee1768203d6d7e0e6d6d99fc23d87a
Ed and Gravy <me@emangini.com>
2023-07-29 13:03:13 -0400

    - removed helm chart for spring cloud gateway - added ingress manifest for templates, and configured for dev and prod envs. - updated ingress paths to enviorment values.yaml - removed config-server from environment Chart.yaml - fixed broken symlinks.

--- product-composite-service/build.gradle.kts
+++ product-composite-service/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation("org.springframework.security:spring-security-oauth2-jose")
     implementation("org.springframework.cloud:spring-cloud-starter-stream-rabbit:4.0.3")
     implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka:4.0.3")
+    implementation("org.springframework.cloud:spring-cloud-commons:4.0.3")
     implementation("io.micrometer:micrometer-tracing-bridge-otel:1.1.2")
     implementation("io.opentelemetry:opentelemetry-exporter-zipkin:1.27.0")
     implementation("org.springframework.retry:spring-retry:2.0.2")
